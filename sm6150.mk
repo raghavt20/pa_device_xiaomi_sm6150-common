bf956194e00f10b2877119f82ded7d90fdf1066e
Shubham Das <dasshubham762@gmail.com>
2020-06-22 09:06:14 +0000

    sm6150-common: Revert some Bluetooth changes
    * Clean Up little bit
    
    Revert "sm6150-common: Switch to QTI bt stack"
    
    This reverts commit dc566a02f72f479825bda6d310b31834ff06f69b.
    
    Revert "sm6150-common: BT: Enable use of QTI stack on the device"
    
    This reverts commit 5c6a9cc03847aebb4f2c5c8914c941f2f55757d3.
    
    Revert "sm6150-common: Disable audio ULL mode"
    
    This reverts commit 44d21d46e50b40e329f00346d66a947fd5a5f095.
    
    Revert "sm6150-common: audio: Switch to CAF audio policy configuration"
    
    This reverts commit 804b5412a25bc84eba1784d3ed736271cf5541ca.
    
    Revert "sm6150-common: BT: Disable AVRCP Target Service from overlay"
    
    This reverts commit aa58a044655bdc96610b443be7382d423bcea04f.
    
    Revert "sm6150-common: Support A2DP offload for Bluetooth Audio HAL V2"
    
    This reverts commit 8219c03590c90e39c8cc22a9f3871d573d333496.

--- sm6150.mk
+++ sm6150.mk
@@ -109,10 +109,11 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
@@ -129,7 +130,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
-    BluetoothQti \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
